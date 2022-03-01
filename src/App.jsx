fe7808391692317d1b7653a03a2fa8806257fac7
Rodolfo de Jesus Ceo Cortes <ceosmarck@gmail.com>
2022-03-01 09:48:52 -0600

    Unico commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Saludo from "./Saludo";
+
+function App() {
+  return (
+    <div className="App">
+      <Saludo/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,9 @@
+const Saludo = () => {
+    return(
+        <h1>Este en mi segundo Hola mundo</h1>
+    );
+
+
+};
+
+export default Saludo;
